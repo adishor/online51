c3a7cb8b67c55227c861af2e6a5dcd9b76552bef
Radu Filip <radu.filip@pitechnologies.ro>
2016-04-05 17:31:34 +0300

    Sonata Admin User show and other translations.

--- src/AppBundle/Admin/UserAdmin.php
+++ src/AppBundle/Admin/UserAdmin.php
@@ -4,65 +4,66 @@ namespace AppBundle\Admin;
 
 use Sonata\UserBundle\Admin\Model\UserAdmin as SonataUserAdmin;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Application\Sonata\UserBundle\Entity\User;
 
 class UserAdmin extends SonataUserAdmin
 {
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         parent::configureFormFields($formMapper);
 
         $formMapper
-            ->removeGroup('Profile', 'User')
-            ->tab('User')
-                ->with('General')
-                    ->add('function', 'choice', array(
-                        'choices' => array(
-                            User::FUNCTION_EXTERN_JOB => 'Serviciu extern',
-                            User::FUNCTION_INTERN_JOB => 'Serviciu intern',
-                            User::FUNCTION_APPOINTED_WORKER => 'Lucrator desemnat',
-                            User::FUNCTION_ADMINISTRATOR => 'Administrator'
-                        )
-                    ))
-                    ->add('credits')
-                ->end()
-                ->with('Profile', array('class' => 'col-md-6'))
-                    ->add('name')
-                    ->add('phone')
-                    ->add('county')
-                    ->add('city')
-                    ->add('address')
-                ->end()
-                ->with('Company', array('class' => 'col-md-6'))
-                    ->add('company')
-                    ->add('uploadImage', 'sonata_admin_image_file', array(
-                        'required' => false
-                    ))
-                    ->add('noEmployees', 'choice', array(
-                        'choices' => array(
-                            User::NO_EMPLOYEES_0_9 => '0-9',
-                            User::NO_EMPLOYEES_10_49 => '10-49',
-                            User::NO_EMPLOYEES_OVER_50 => 'peste 50'
-                        ),
-                        'empty_value' => 'Selectati Nr de angajati',
-                        'required' => false
-                    ))
-                    ->add('cui')
-                    ->add('bank')
-                    ->add('iban')
-                    ->add('noRegistrationORC')
-                    ->add('noCertifiedEmpowerment')
-                ->end()
-            ->end()
-            ->removeGroup('Social', 'User');
+          ->removeGroup('Profile', 'User')
+          ->tab('User')
+          ->with('General')
+          ->add('function', 'choice', array(
+              'choices' => array(
+                  User::FUNCTION_EXTERN_JOB => 'Serviciu extern',
+                  User::FUNCTION_INTERN_JOB => 'Serviciu intern',
+                  User::FUNCTION_APPOINTED_WORKER => 'Lucrator desemnat',
+                  User::FUNCTION_ADMINISTRATOR => 'Administrator'
+              )
+          ))
+          ->add('credits')
+          ->end()
+          ->with('Profile', array('class' => 'col-md-6'))
+          ->add('name')
+          ->add('phone')
+          ->add('county')
+          ->add('city')
+          ->add('address')
+          ->end()
+          ->with('Company', array('class' => 'col-md-6'))
+          ->add('company')
+          ->add('uploadImage', 'sonata_admin_image_file', array(
+              'required' => false
+          ))
+          ->add('noEmployees', 'choice', array(
+              'choices' => array(
+                  User::NO_EMPLOYEES_0_9 => '0-9',
+                  User::NO_EMPLOYEES_10_49 => '10-49',
+                  User::NO_EMPLOYEES_OVER_50 => 'peste 50'
+              ),
+              'empty_value' => 'Selectati Nr de angajati',
+              'required' => false
+          ))
+          ->add('cui')
+          ->add('bank')
+          ->add('iban')
+          ->add('noRegistrationORC')
+          ->add('noCertifiedEmpowerment')
+          ->end()
+          ->end()
+          ->removeGroup('Social', 'User');
     }
 
     public function getTemplate($name)
     {
-        if ( $name == "edit" )
-        {
-            return 'sonata/user/base_edit.html.twig' ;
+        if ($name == "edit") {
+            return 'sonata/user/base_edit.html.twig';
         }
         return parent::getTemplate($name);
     }
@@ -75,9 +76,52 @@ class UserAdmin extends SonataUserAdmin
     public function getFormTheme()
     {
         return array_merge(
-            parent::getFormTheme(),
-            array('sonata/admin_image_file_field.html.twig')
+          parent::getFormTheme(), array('sonata/admin_image_file_field.html.twig')
         );
     }
-}
 
+    protected function configureShowFields(ShowMapper $show)
+    {
+        $show
+          ->tab('User')
+          ->with('General')
+          ->add('email')
+          ->add('function', 'choice', array(
+              'choices' => array(
+                  User::FUNCTION_EXTERN_JOB => 'Serviciu extern',
+                  User::FUNCTION_INTERN_JOB => 'Serviciu intern',
+                  User::FUNCTION_APPOINTED_WORKER => 'Lucrator desemnat',
+                  User::FUNCTION_ADMINISTRATOR => 'Administrator'
+              )
+          ))
+          ->add('credits')
+          ->end()
+          ->with('Profile')
+          ->add('name')
+          ->add('phone')
+          ->add('county')
+          ->add('city')
+          ->add('address')
+          ->end()
+          ->with('Company')
+          ->add('company')
+          ->add('uploadImage', 'sonata_admin_image_file', array(
+              'template' => 'sonata/show_admin_image_file_field.html.twig'
+          ))
+          ->add('noEmployees', 'choice', array(
+              'choices' => array(
+                  User::NO_EMPLOYEES_0_9 => '0-9',
+                  User::NO_EMPLOYEES_10_49 => '10-49',
+                  User::NO_EMPLOYEES_OVER_50 => 'peste 50'
+              )))
+          ->add('cui')
+          ->add('bank')
+          ->add('iban')
+          ->add('noRegistrationORC')
+          ->add('noCertifiedEmpowerment')
+          ->end()
+          ->end()
+        ;
+    }
+
+}
