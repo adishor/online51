d283a288d9b2d1c2862be918e2608cf2f9d2f642
Radu Filip <radu.filip@pitechnologies.ro>
2016-04-08 11:30:09 +0300

    User server validation and translations.

--- src/AppBundle/Form/Type/RegisterType.php
+++ src/AppBundle/Form/Type/RegisterType.php
@@ -9,50 +9,48 @@ use Application\Sonata\UserBundle\Entity\User;
 
 class RegisterType extends AbstractType
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', null, array(
-                    'required' => true,
-                ))
-                ->add('function', 'choice', array(
-                    'required' => true,
-                    'choices' => array(
-                        User::FUNCTION_EXTERN_JOB => 'Serviciu extern',
-                        User::FUNCTION_INTERN_JOB => 'Serviciu intern',
-                        User::FUNCTION_APPOINTED_WORKER => 'Lucrator desemnat',
-                        User::FUNCTION_ADMINISTRATOR => 'Administrator'
-                    )
-                ))
-                ->add('email', 'text')
-                ->add('password', 'repeated', array(
-                    'type' => 'password',
-                    'required' => true
-                ))
-                ->add('company')
-                ->add('cui')
-                ->add('noRegistrationORC')
-                ->add('noEmployees', 'choice', array(
-                    'required' => true,
-                    'choices' => array(
-                        User::NO_EMPLOYEES_0_9 => '0-9',
-                        User::NO_EMPLOYEES_10_49 => '10-49',
-                        User::NO_EMPLOYEES_OVER_50 => 'peste 50'
-                    ),
-                    'empty_value' => 'Selectati Nr de angajati'
-                ))
-                ->add('noCertifiedEmpowerment')
-                ->add('bank')
-                ->add('iban')
-                ->add('phone')
-                ->add('county', null, array(
-                    'empty_value' => 'Selectati judetul'
-                ))
-                ->add('city', null, array(
-                    'required' => false
-                ))
-                ->add('address')
-                ->add('uploadImage', 'file')
-                ;
+        $builder
+          ->add('name')
+          ->add('function', 'choice', array(
+              'required' => true,
+              'choices' => array(
+                  User::FUNCTION_EXTERN_JOB => 'Serviciu extern',
+                  User::FUNCTION_INTERN_JOB => 'Serviciu intern',
+                  User::FUNCTION_APPOINTED_WORKER => 'Lucrator desemnat',
+                  User::FUNCTION_ADMINISTRATOR => 'Administrator'
+              )
+          ))
+          ->add('email', 'text')
+          ->add('password', 'repeated', array(
+              'type' => 'password',
+              'required' => true,
+              'invalid_message' => 'assert.password.same',
+          ))
+          ->add('company')
+          ->add('cui')
+          ->add('noRegistrationORC')
+          ->add('noEmployees', 'choice', array(
+              'choices' => array(
+                  User::NO_EMPLOYEES_0_9 => '0-9',
+                  User::NO_EMPLOYEES_10_49 => '10-49',
+                  User::NO_EMPLOYEES_OVER_50 => 'peste 50'
+              ),
+              'empty_value' => 'Selectati Nr de angajati'
+          ))
+          ->add('noCertifiedEmpowerment')
+          ->add('bank')
+          ->add('iban')
+          ->add('phone')
+          ->add('county', null, array(
+              'empty_value' => 'Selectati judetul'
+          ))
+          ->add('city')
+          ->add('address')
+          ->add('uploadImage', 'file')
+        ;
     }
 
     /**
@@ -71,4 +69,3 @@ class RegisterType extends AbstractType
         return 'register';
     }
 }
-
